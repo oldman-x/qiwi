986fa8198ebd25d4bcb02909e1668f7b349770af
oldman-x <46452901+oldman-x@users.noreply.github.com>
2019-02-07 18:21:20 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright 2019 oldman-x <oldman-x@xmpp.jp>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+ 
+return (object) array(
+// Define all Qiwi wallets
+	// First Qiwi wallet
+	'qiwi_1' => (object) array(
+		'api_key' => 'YUu2qw048gtdsvlk3iu',
+		'phone_number' => '9121112233',
+		'phone_prefix' => '7',
+	)
+	
+	// Second Qiwi wallet
+	'qiwi_2' => (object) array(
+		'api_key' => 'YUu2qw048gtdsvlk3iu',
+		'phone_number' => '9121112233',
+		'phone_prefix' => '7',
+	)
+	
+	// n Qiwi wallet
+	
+);
+
+?>
